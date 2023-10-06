cc50ef1052693449c8caa56cbf28d05faf7fbca7
Shir hamawie <93772012+shirHamawie@users.noreply.github.com>
2023-10-06 19:20:45 +0300

    Update Wolt Codes Extractor.py

--- Wolt Codes Extractor.py
+++ Wolt Codes Extractor.py
@@ -1,3 +1,4 @@
+import traceback
 import win32com.client
 from datetime import datetime, timedelta
 import PyPDF2
@@ -13,12 +14,13 @@ output_file = True
 printing = True
 delete_mail = True
 clean_file = False
+debug_mode = True
 save_pdf_path = "C:\\Users\\t-shamawie\\Downloads"
 attachment_symbol = "english"
 code_pattern = r'CODE:\s+(\w+)'
 valid_until_pattern = r'Valid until:\s+([A-Za-z]+\s+\d{1,2},\s+\d{4})'
 amount_pattern = r'₪\s+([\d.]+)'
-dates_in_past_30_days = []
+unhandled_dates = []
 counter = 0
 earnings = 0
 
@@ -40,22 +42,33 @@ def iterate_mailbox(folder):
 
 
 def handle_attachments(item):
+    if item.Attachments.Count == 0:
+        return False
+
     for attachment in item.Attachments:
         attachment_name = attachment.FileName.lower()
+
         if attachment_name.endswith(".pdf") and attachment_symbol in attachment_name:
-            pdf_path = os.path.join(save_pdf_path, attachment.FileName)
-            attachment.SaveAsFile(pdf_path)
-            with open(pdf_path, "rb") as pdf_file:
+            try:
+                pdf_path = os.path.join(save_pdf_path, attachment.FileName)
+                attachment.SaveAsFile(pdf_path)
+
+                pdf_file = open(pdf_path, "rb")
                 pdf_reader = PyPDF2.PdfReader(pdf_file)
                 text = extract_text(pdf_reader)
-                success = handle_code(text)
-                global counter
-                counter += 1
-            if success:
-                os.remove(pdf_path)
-        else:
-            success = True
-    return success
+
+                if handle_code(text):
+                    global counter
+                    counter += 1
+                    pdf_file.close()
+                    os.remove(pdf_path)
+                    return True
+
+            except Exception as e:
+                print_error(e)
+                return False
+
+    return False
 
 
 def handle_mails(folder_name):
@@ -73,22 +86,23 @@ def extract_text(pdf_reader):
 
 
 def write_to_file(code, date, amount):
-    with open(file_path, 'a', encoding='utf-8') as file:
-        try:
+    try:
+        with open(file_path, 'a', encoding='utf-8') as file:
             file.write(f"Amount: {amount}₪\n")
             file.write(f"Code: {code}\n")
             file.write(f"Date: {date}\n")
             file.write("-" * 26 + "\n")
-        except Exception as e:
-            print_error(e)
-            if not printing:
-                print_values_blue(code, date, amount)
+    except Exception as e:
+        print_error(e)
+        if not printing:
+            print_values_blue(code, date, amount)
 
 
 def handle_code(text):
     code_match = re.search(code_pattern, text)
     valid_until_match = re.search(valid_until_pattern, text)
     amount_match = re.search(amount_pattern, text)
+    ret = True
 
     if amount_match:
         amount = amount_match.group(1)
@@ -96,11 +110,13 @@ def handle_code(text):
         earnings += float(amount)
     else:
         amount = "Amount not found"
+        ret = False
 
     if code_match:
         code = code_match.group(1)
     else:
         code = "Code not found"
+        ret = False
 
     if valid_until_match:
         valid_until = valid_until_match.group(1).replace(',', '')
@@ -108,20 +124,21 @@ def handle_code(text):
         date = date.replace(year=int(date.year - 5))
         day = date.strftime("%A")
         date = date.strftime('%b %d %Y')
-        global dates_in_past_30_days
-        if date in dates_in_past_30_days:
-            dates_in_past_30_days.remove(date)
+        global unhandled_dates
+        if date in unhandled_dates:
+            unhandled_dates.remove(date)
 
     else:
         date = "Valid until date not found"
         day = ""
+        ret = False
 
     if output_file:
         write_to_file(code, date + " " + day, amount)
     if printing:
         print_values_blue(code, date + " " + day, amount)
 
-    return True
+    return ret
 
 
 def print_values_blue(code, date, amount):
@@ -140,38 +157,36 @@ def print_pink(to_print, end='\n'):
     print('\033[95m', to_print, end=end)
 
 
-def handled_dates():
+def find_handled_dates():
     date_pattern = r'Date: (\w{3} \d{2} \d{4})'
 
-    with open(file_path, 'r') as file:
-        text = file.read()
-        date_matches = re.findall(date_pattern, text)
+    try:
+        with open(file_path, 'r') as file:
+            text = file.read()
+            date_matches = re.findall(date_pattern, text)
+    except Exception as e:
+        print_error(e)
 
-    date_list = []
-
-    for date_match in date_matches:
-        date_obj = datetime.strptime(date_match, '%b %d %Y')
-        date = date_obj.strftime('%b %d %Y')
-        date_list.append(date)
-
-    return date_list
+    return [datetime.strptime(date_match, '%b %d %Y').strftime('%b %d %Y') for date_match in date_matches]
 
 
 def manipulate_dates():
-    global dates_in_past_30_days
+    global unhandled_dates
+
     current_date = datetime.now()
-    thirty_days_ago = current_date - timedelta(days=30)
+    last_days_ago = current_date - timedelta(days=70)
     current_day = current_date
-    while current_day >= thirty_days_ago:
+
+    while current_day >= last_days_ago:
         day = current_day.strftime("%A")
         if day != "Friday" and day != "Saturday":
-            dates_in_past_30_days.append(current_day.strftime('%b %d %Y'))
+            unhandled_dates.append(current_day.strftime('%b %d %Y'))
         current_day -= timedelta(days=1)
 
-    handled_dates_list = handled_dates()
+    handled_dates_list = find_handled_dates()
     for date in handled_dates_list:
-        if date in dates_in_past_30_days:
-            dates_in_past_30_days.remove(date)
+        if date in unhandled_dates:
+            unhandled_dates.remove(date)
 
 
 def generate_date_list(month):
@@ -194,7 +209,7 @@ def generate_date_list(month):
 
 
 def print_calender(month):
-    global dates_in_past_30_days
+    global unhandled_dates
 
     today = datetime.now()
 
@@ -203,14 +218,21 @@ def print_calender(month):
     print_pink("\t\t" + "--- " + datetime.strptime(str(month), '%m').strftime('%B') + " ---")
     print_pink('Sun  Mon  Tue  Wed  Thu  Fri  Sat')
 
+    first_day_of_last_month = (today.replace(day=1) - timedelta(days=1)) \
+        .replace(day=1) \
+        .replace(hour=0, minute=0, second=0, microsecond=0)
+
     for i in range(len(date_list)):
         date = date_list[i]
         date_obj = date_objects[i]
-        day = date_obj.strftime('%d')
 
-        if today - date_obj > timedelta(days=30):
-            print_pink(' P  ', end='')
-        elif date in dates_in_past_30_days:
+        day = date_obj.strftime('%d')
+        if day.startswith('0'):
+            day = day[1:]
+
+        if date_obj < first_day_of_last_month:
+            print_pink('    ', end='')
+        elif date in unhandled_dates:
             print_pink(' X  ', end='')
         else:
             if datetime.strptime(date, '%b %d %Y').month != month:
@@ -228,19 +250,23 @@ def print_calender(month):
     print()
 
 
-def generate_month_ago_dates():
+def generate_dates_list(days):
     today = datetime.now()
-    return [today - timedelta(days=i) for i in range(30)]
+    return [today - timedelta(days=i) for i in range(days)]
 
 
 def print_calenders():
-    months_list = set()
+    months_set = set()
 
-    for date in generate_month_ago_dates():
-        months_list.add(date.month)
+    for date in generate_dates_list(60):
+        months_set.add(date.month)
 
-    months_list = sorted(months_list)
-    for month in months_list:
+    if len(months_set) > 2:
+        max_difference_month = max(months_set, key=lambda x: abs(x - datetime.now().month))
+        months_set.remove(max_difference_month)
+
+    months_set = sorted(months_set)
+    for month in months_set:
         print_calender(month)
 
 
@@ -248,8 +274,23 @@ def alert_popup(add):
     root = tk.Tk()
     root.withdraw()
 
-    confirmation = messagebox.askyesnocancel("Confirmation", add + "!\nAre you sure you want to continue?",
+    confirmation = messagebox.askyesnocancel("Warning", add + "!\nAre you sure you want to continue?\n "
+                                                              "Press 'Yes' to proceed, 'No' to turn the mode off, "
+                                                              "or 'Cancel' to exit the program",
                                              icon=messagebox.WARNING)
+    root.destroy()
+
+    return confirmation
+
+
+def debug_alert():
+    root = tk.Tk()
+    root.withdraw()
+
+    confirmation = messagebox.askokcancel("Confirmation",
+                                          "You are on debug mode!\nOutput file, "
+                                          "deleting mails and clean file is turned off",
+                                          icon=messagebox.INFO)
     root.destroy()
 
     return confirmation
@@ -261,6 +302,11 @@ def run():
         global clean_file
         global output_file
         global outlook
+
+        if debug_mode:
+            status = debug_alert()
+            if status:
+                output_file = delete_mail = clean_file = False
 
         if delete_mail:
             delete = alert_popup("You are about to delete the mails")
@@ -298,11 +344,12 @@ def run():
             del outlook
         if printing:
             print_green(str(counter), "Mails handled")
-            print_green(str(earnings) + "₪", "Earnings")
+            print_green(str(int(earnings)) + "₪", "Earnings")
             print()
             print_calenders()
     except Exception as e:
         print_error(e)
+        # print_error(traceback.format_exc())
 
 
 run()
